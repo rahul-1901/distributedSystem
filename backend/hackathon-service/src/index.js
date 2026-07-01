e3d439e17a056f5137a044b1e3710336c52d3aa3
rahul-1901 <iitdelhi98@gmail.com>
2026-07-01 21:11:25 +0530

    awsChangesToPush

--- backend/hackathon-service/src/index.js
+++ backend/hackathon-service/src/index.js
@@ -21,8 +21,8 @@ dotenv.config();
 
 if (
   !process.env.AWS_REGION ||
-  !process.env.AWS_ACCESS_KEY_ID ||
-  !process.env.AWS_SECRET_ACCESS_KEY ||
+  // !process.env.AWS_ACCESS_KEY_ID ||
+  // !process.env.AWS_SECRET_ACCESS_KEY ||
   !process.env.AWS_S3_BUCKET_NAME
 ) {
   throw new Error("AWS configuration missing");
